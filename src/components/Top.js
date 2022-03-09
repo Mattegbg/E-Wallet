23b953e0d60864623f9b7eb3d2cb9cb38ef6940d
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-03-10 00:53:06 +0100

    Initial commit

---
+++ src/components/Top.js
@@ -0,0 +1,8 @@
+
+import React from 'Top';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>, <h1> Hello2, {props.name2} </h1>;
+  }
+
+  export default Welcome;
---
+++ src/views/Home.js
@@ -0,0 +1,9 @@
+
+
+function Home() {
+    return (
+        <h1>Detta är startsidan</h1>
+    )
+}
+
+export default Home;
